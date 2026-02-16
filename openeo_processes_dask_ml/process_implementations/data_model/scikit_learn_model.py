b09fa316150775cbc211a1be1b91319d4e6bbdbb
= <jonhur@web.de>
2026-02-16 14:39:33 +0100

    RF fitting

--- openeo_processes_dask_ml/process_implementations/data_model/scikit_learn_model.py
+++ openeo_processes_dask_ml/process_implementations/data_model/scikit_learn_model.py
@@ -1,8 +1,15 @@
+import copy
 import pickle
 from collections.abc import Iterable
 from pathlib import Path
+from typing import Self
 
+import xarray as xr
+from dask import dataframe as ddf
+from dask import delayed
+from pystac.extensions.classification import Classification
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
 from openeo_processes_dask_ml.process_implementations.constants import MODEL_CACHE_DIR
 
@@ -37,3 +44,52 @@ class RfClassModel(SkLearnModel):
             pickle.dump(r, file)
 
         return modelpath
+
+    @delayed
+    def fit(self, training_set_df: ddf.DataFrame) -> str:
+        print("fitting model")
+
+        model_path = self._model_filepath
+
+        with open(model_path, "rb") as file:
+            model: RandomForestClassifier = pickle.load(file)
+
+        X_train = training_set_df[self.input.bands]
+        y_train = training_set_df[self.output.result.dim_order[0]]
+
+        encoder = LabelEncoder()
+        y_train_enc = encoder.fit_transform(y_train)
+
+        model.fit(X_train, y_train_enc)
+
+        self.output.classes = [
+            Classification.create(i, name=name)
+            for i, name in enumerate(encoder.classes_)
+        ]
+
+        with open(model_path, "wb") as file:
+            pickle.dump(model, file)
+
+        print("fitted model")
+
+        return model_path
+
+    def fit_model(self, training_set: xr.DataArray) -> Self:
+        in_dims = self.input.input.dim_order
+        if len(in_dims) > 1:
+            raise ValueError("Only one input dimension is allowed in RF classifier")
+        in_dim = in_dims[0]
+
+        out_dims = self.output.result.dim_order
+        if len(out_dims) > 1:
+            raise ValueError("Only one output dimension is allowed in RF classifier")
+
+        training_set_ds = training_set.to_dataset(dim=in_dim)
+        training_set_df = training_set_ds.to_dask_dataframe()
+
+        fitted_model_path = self.fit(training_set_df)
+
+        rf_model_copy = copy.deepcopy(self)
+        rf_model_copy._model_filepath = fitted_model_path
+
+        return rf_model_copy
