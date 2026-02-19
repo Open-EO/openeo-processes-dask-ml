9ce8055846f21215113f08dbfa5a88f146c97d81
= <jonhur@web.de>
2026-02-19 11:40:44 +0100

    new process graph

--- examples/train_rf.py
+++ examples/train_rf.py
@@ -17,87 +17,143 @@ with open("examples/training_data/train_data_small.json") as file:
 
 
 process_graph = {
-    "process_graph": {
-        "load_data": {
-            "process_id": "load_stac",
-            "arguments": {
-                "url": "https://earth-search.aws.element84.com/v1/collections/sentinel-2-l2a",
-                "spatial_extent": {
-                    "west": -2.96,
-                    "east": -2.7,
-                    "south": 47.91,
-                    "north": 48.06,
-                },
-                "temporal_extent": ["2022-01-01", "2022-02-28"],
-                "bands": [
-                    "coastal",
-                    "blue",
-                    "green",
-                    "red",
-                    "rededge1",
-                    "rededge2",
-                    "rededge3",
-                    "nir",
-                    "nir08",
-                    "nir09",
-                    "swir16",
-                    "swir22",
-                ],
-                "resolution": 10,
+    "loadcollection1": {
+        "process_id": "load_collection",
+        "arguments": {
+            "bands": ["blue", "green", "red", "nir"],
+            "id": "sentinel-2-l2a",
+            "spatial_extent": {
+                "west": -2.997065,
+                "south": 47.892255,
+                "east": -2.671039,
+                "north": 48.047242,
+                "crs": 4326,
             },
-        },
-        "agg_temp": {
-            "process_id": "aggregate_temporal_period",
-            "arguments": {
-                "data": {"from_node": "load_data"},
-                "period": "month",
-                "reducer": {
-                    "process_graph": {
-                        "median1": {
-                            "process_id": "median",
-                            "arguments": {"data": {"from_parameter": "data"}},
-                            "result": True,
-                        }
-                    }
-                },
-            },
-        },
-        "ndvi": {
-            "process_id": "ndvi",
-            "arguments": {"data": {"from_node": "agg_temp"}, "target_band": "NDVI"},
-        },
-        "training_data": {
-            "process_id": "aggregate_spatial",
-            "arguments": {
-                "data": {"from_node": "ndvi"},
-                "geometries": geoms,
-                "reducer": {
-                    "process_graph": {
-                        "median1": {
-                            "process_id": "median",
-                            "arguments": {"data": {"from_parameter": "data"}},
-                            "result": True,
-                        }
-                    }
-                },
-            },
-        },
-        "rf_init": {
-            "process_id": "mlm_class_random_forest",
-            "arguments": {"max_variables": "sqrt", "num_trees": 50},
-        },
-        "fit": {
-            "process_id": "ml_fit",
-            "arguments": {
-                "model": {"from_node": "rf_init"},
-                "training_set": {"from_node": "training_data"},
-                "target": "class_name",
-            },
-            "result": True,
+            "temporal_extent": ["2017-03-01", "2017-05-30"],
         },
     },
-    "parameters": [],
+    "aggregatetemporalperiod1": {
+        "process_id": "aggregate_temporal_period",
+        "arguments": {
+            "data": {"from_node": "loadcollection1"},
+            "period": "month",
+            "reducer": {
+                "process_graph": {
+                    "median1": {
+                        "process_id": "median",
+                        "arguments": {"data": {"from_parameter": "data"}},
+                        "result": True,
+                    }
+                }
+            },
+        },
+    },
+    "ndvi1": {
+        "process_id": "ndvi",
+        "arguments": {
+            "data": {"from_node": "aggregatetemporalperiod1"},
+            "nir": "nir",
+            "red": "red",
+            "target_band": "NDVI",
+        },
+    },
+    "arrayinterpolatelinear1": {
+        "process_id": "array_interpolate_linear",
+        "arguments": {"data": {"from_node": "ndvi1"}},
+    },
+    "mlmclassrandomforest1": {
+        "process_id": "mlm_class_random_forest",
+        "arguments": {"max_variables": "sqrt", "num_trees": 200, "seed": 42},
+    },
+    "loadcollection2": {
+        "process_id": "load_collection",
+        "arguments": {
+            "bands": ["green", "blue", "red", "nir"],
+            "id": "sentinel-2-l2a",
+            "spatial_extent": {
+                "west": -4.026661,
+                "south": 48.202738,
+                "east": -3.743587,
+                "north": 48.300371,
+                "crs": 4326,
+            },
+            "temporal_extent": ["2017-03-01", "2017-05-30"],
+        },
+    },
+    "aggregatetemporalperiod2": {
+        "process_id": "aggregate_temporal_period",
+        "arguments": {
+            "data": {"from_node": "loadcollection2"},
+            "period": "month",
+            "reducer": {
+                "process_graph": {
+                    "median2": {
+                        "process_id": "median",
+                        "arguments": {"data": {"from_parameter": "data"}},
+                        "result": True,
+                    }
+                }
+            },
+        },
+    },
+    "ndvi2": {
+        "process_id": "ndvi",
+        "arguments": {
+            "data": {"from_node": "aggregatetemporalperiod2"},
+            "nir": "nir",
+            "red": "red",
+            "target_band": "NDVI",
+        },
+    },
+    "arrayinterpolatelinear2": {
+        "process_id": "array_interpolate_linear",
+        "arguments": {"data": {"from_node": "ndvi2"}},
+    },
+    "aggregatespatial1": {
+        "process_id": "aggregate_spatial",
+        "arguments": {
+            "data": {"from_node": "arrayinterpolatelinear2"},
+            "geometries": geoms,
+            "reducer": {
+                "process_graph": {
+                    "mean1": {
+                        "process_id": "mean",
+                        "arguments": {"data": {"from_parameter": "data"}},
+                        "result": True,
+                    }
+                }
+            },
+        },
+    },
+    "mlfit1": {
+        "process_id": "ml_fit",
+        "arguments": {
+            "model": {"from_node": "mlmclassrandomforest1"},
+            "target": "class_name",
+            "training_set": {"from_node": "aggregatespatial1"},
+        },
+    },
+    "mlpredict1": {
+        "process_id": "ml_predict",
+        "arguments": {
+            "data": {"from_node": "arrayinterpolatelinear1"},
+            "model": {"from_node": "mlfit1"},
+        },
+    },
+    "saveresult1": {
+        "process_id": "save_result",
+        "arguments": {
+            "data": {"from_node": "mlpredict1"},
+            "format": "GTiff",
+            "options": {},
+        },
+        "result": True,
+    },
 }
 
+
 out = execute_graph_dict(process_graph)
-# print(out)
+
+# out = out.compute()
+
+print(out)
