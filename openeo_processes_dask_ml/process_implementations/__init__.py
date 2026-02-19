454504383b9172bac4ca7ffc5a229dc22a8c6e2a
= <jonhur@web.de>
2026-02-19 13:53:40 +0100

    init save_ml_model process

--- openeo_processes_dask_ml/process_implementations/__init__.py
+++ openeo_processes_dask_ml/process_implementations/__init__.py
@@ -2,3 +2,4 @@ from .init_models import mlm_class_random_forest
 from .load_model import load_stac_ml
 from .ml_fit import ml_fit
 from .ml_predict import ml_predict
+from .save_ml_model import save_ml_model
---
+++ openeo_processes_dask_ml/process_implementations/save_ml_model.py
@@ -0,0 +1,5 @@
+from openeo_processes_dask_ml.process_implementations.data_model import MLModel
+
+
+def save_ml_model(data: MLModel, name: str, options: dict = None):
+    pass
