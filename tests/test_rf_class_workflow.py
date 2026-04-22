c808129b6fe6c160f48c8bdc2c36b3f6a02b2af9
= <jonhur@web.de>
2026-04-22 17:01:08 +0200

    adjust tests

--- tests/test_rf_class_workflow.py
+++ tests/test_rf_class_workflow.py
@@ -1,7 +1,6 @@
-import builtins
 import os
-from unittest.mock import patch
 
+import numpy as np
 import pytest
 import xarray as xr
 from dask import array as da
@@ -14,16 +13,18 @@ from openeo_processes_dask_ml.process_implementations import (
 )
 from openeo_processes_dask_ml.process_implementations.constants import MODEL_CACHE_DIR
 
+TARGET_DIM = "target_dim"
+
 
 @pytest.fixture
 def training_data() -> xr.DataArray:
     data = xr.DataArray(
         da.random.random((999, 5, 12)),
-        dims=("geometry", "time", "bands"),
+        dims=("geometry", "time", TARGET_DIM),
         coords={
             "geometry": range(999),
             "class_name": ("geometry", [*333 * ["a"], *333 * ["b"], *333 * ["c"]]),
-            "bands": [f"B{str(b)}" for b in range(12)],
+            TARGET_DIM: [f"B{str(b)}" for b in range(12)],
         },
     )
     data.name = None
@@ -34,9 +35,9 @@ def training_data() -> xr.DataArray:
 def predict_data() -> xr.DataArray:
     dc = xr.DataArray(
         da.random.random((5, 12, 10, 10)),
-        dims=("time", "band", "x", "y"),
+        dims=("time", TARGET_DIM, "x", "y"),
         coords={
-            "band": [f"B{str(b)}" for b in range(12)],
+            TARGET_DIM: [f"B{str(b)}" for b in range(12)],
         },
     )
     dc.name = None
@@ -89,13 +90,22 @@ def test_init_model_no_temporal():
 # this is by no means systematic testing, or modular or anything
 # shoild add proper systematic testing later, but this is better than nothing
 @pytest.mark.vcr()
+@pytest.mark.parametrize("dim_to_classify", (None, "band", "embedding"))
 def test_fit_model_default_values(
-    training_data: xr.DataArray, predict_data: xr.DataArray
+    training_data: xr.DataArray, predict_data: xr.DataArray, dim_to_classify: str
 ):
     # 1) init model
-    model = mlm_class_random_forest("log2")
+    if dim_to_classify is None:
+        model = mlm_class_random_forest("log2")
+        dim_to_classify = "bands"
+    else:
+        model = mlm_class_random_forest("log2", dimension=dim_to_classify)
 
     untrained_filepath = model._model_filepath
+
+    # rename dimensions
+    training_data = training_data.rename({TARGET_DIM: dim_to_classify})
+    predict_data = predict_data.rename({TARGET_DIM: dim_to_classify})
 
     with open(untrained_filepath, "rb") as file:
         untrained_model_bytes = file.read()
@@ -103,10 +113,11 @@ def test_fit_model_default_values(
     # 2) fit model
     fitted = ml_fit(model, training_data, "class_name")
 
-    assert fitted.input.input.dim_order == ["time", "bands"]
+    assert len(fitted.input.input.dim_order) == 2
+    assert fitted.input.input.dim_order == ["time", dim_to_classify]
     assert fitted.input.input.shape == [
         len(training_data.coords["time"]),
-        len(training_data.coords["bands"]),
+        len(training_data.coords[dim_to_classify]),
     ]
 
     assert fitted.input.input.shape == [5, 12]
@@ -116,6 +127,14 @@ def test_fit_model_default_values(
 
     assert fitted.model_metadata.pretrained == True
 
+    if dim_to_classify in ["band", "bands"]:
+        assert len(fitted.input.bands) == len(training_data.coords[dim_to_classify])
+        np.testing.assert_array_equal(
+            fitted.input.bands, training_data.coords[dim_to_classify].values
+        )
+    else:
+        assert len(fitted.input.bands) == 0
+
     assert isinstance(fitted._model_filepath, Delayed)
 
     out_path = fitted._model_filepath.compute()
@@ -133,14 +152,18 @@ def test_fit_model_default_values(
     out = ml_predict(predict_data, fitted)
 
     assert isinstance(out, xr.DataArray)
-    assert "bands" not in out.dims
+    assert dim_to_classify not in out.dims
     assert "time" not in out.dims
 
     assert "class_name" in out.dims
     assert len(out.coords["class_name"]) == 1
 
-    assert len(predict_data.coords["x"]) == len(out.coords["x"])
-    assert len(predict_data.coords["y"]) == len(out.coords["y"])
+    np.testing.assert_array_equal(
+        predict_data.coords["x"].values, out.coords["x"].values
+    )
+    np.testing.assert_array_equal(
+        predict_data.coords["y"].values, out.coords["y"].values
+    )
 
     out = out.compute()
     assert isinstance(out, xr.DataArray)
@@ -149,11 +172,24 @@ def test_fit_model_default_values(
 
 
 @pytest.mark.vcr()
-def test_fit_model_no_temporal(training_data: xr.DataArray, predict_data: xr.DataArray):
+@pytest.mark.parametrize("dim_to_classify", (None, "band", "embedding"))
+def test_fit_model_no_temporal(
+    training_data: xr.DataArray, predict_data: xr.DataArray, dim_to_classify: str
+):
     # 1) init model
-    model = mlm_class_random_forest("log2", use_timeseries=False)
+    if dim_to_classify is None:
+        model = mlm_class_random_forest("log2", use_timeseries=False)
+        dim_to_classify = "bands"
+    else:
+        model = mlm_class_random_forest(
+            "log2", dimension=dim_to_classify, use_timeseries=False
+        )
 
     untrained_filepath = model._model_filepath
+
+    # rename dimensions
+    training_data = training_data.rename({TARGET_DIM: dim_to_classify})
+    predict_data = predict_data.rename({TARGET_DIM: dim_to_classify})
 
     with open(untrained_filepath, "rb") as file:
         untrained_model_bytes = file.read()
@@ -162,10 +198,18 @@ def test_fit_model_no_temporal(training_data: xr.DataArray, predict_data: xr.Dat
     fitted = ml_fit(model, training_data, "class_name")
 
     assert len(fitted.input.input.dim_order) == 1
-    assert fitted.input.input.dim_order == ["bands"]
+    assert fitted.input.input.dim_order == [dim_to_classify]
     assert "time" not in fitted.input.input.dim_order
 
     assert fitted.input.input.shape == [12]
+
+    if dim_to_classify in ["band", "bands"]:
+        assert len(fitted.input.bands) == len(training_data.coords[dim_to_classify])
+        np.testing.assert_array_equal(
+            fitted.input.bands, training_data.coords[dim_to_classify].values
+        )
+    else:
+        assert len(fitted.input.bands) == 0
 
     assert isinstance(fitted._model_filepath, Delayed)
 
@@ -184,19 +228,24 @@ def test_fit_model_no_temporal(training_data: xr.DataArray, predict_data: xr.Dat
     out = ml_predict(predict_data, fitted)
 
     assert isinstance(out, xr.DataArray)
-    assert "bands" not in out.dims
+    assert dim_to_classify not in out.dims
     assert "time" in out.dims
 
     assert "class_name" in out.dims
     assert len(out.coords["class_name"]) == 1
     assert len(out.coords["time"]) == 5
 
-    assert len(predict_data.coords["x"]) == len(out.coords["x"])
-    assert len(predict_data.coords["y"]) == len(out.coords["y"])
+    np.testing.assert_array_equal(
+        predict_data.coords["x"].values, out.coords["x"].values
+    )
+    np.testing.assert_array_equal(
+        predict_data.coords["y"].values, out.coords["y"].values
+    )
+    np.testing.assert_array_equal(
+        predict_data.coords["time"].values, out.coords["time"].values
+    )
 
     out = out.compute()
     assert isinstance(out, xr.DataArray)
-
-    print(out)
 
     os.remove(out_path)
